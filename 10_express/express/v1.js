2ee8f16b00aa26b6d93374578d74c05ff3c9090c
szy0syz <szy0syz@gmail.com>
2019-03-13 23:20:04 +0800

    Update 10_express v2

--- 10_express/express/v1.js
+++ 10_express/express/v1.js
@@ -1,6 +1,5 @@
 const url = require('url');
 const http = require('http');
-const methods = require('methods');
 
 function application() {
   let app = (req, res) => {
@@ -9,31 +8,27 @@ function application() {
     for (let i = 0; i < app.routes.length; i++) {
       const layer = app.routes[i];
       const { path, handler, method } = layer;
-
-      if ((pathname === path || path === '*') && ((requestMethod === method) || method === 'all')) {
+      if (pathname === path && requestMethod === method) {
         // 如果找到了对应的路径与请求方法，就执行回调并返回，不再往下找
         return handler(req, res);
       }
     }
-    // 如果没有任何路由被匹配就执行 默认方法
+    // 如果没有任何路由被匹配就执行默认方法
     res.end(`Cannot ${requestMethod.toUpperCase()} ${pathname}`);
   }
 
   // 存路由
   app.routes = [];
 
-  // 遍历所有可能存在的http请求方法，并挂载到app这个函数上，使得我们在用.get/.delete/.head添加监听函数时都能与之对应到正确的http方法
-  // 同时还需加入all方法
-  [...methods, 'all'].forEach(method => {
-    app[method] = function (path, handler) {
-      let layer = {
-        path,
-        method,
-        handler,
-      };
-      app.routes.push(layer);
-    }
-  })
+  // 保存 get 请求方法的操作
+  app.get = function (path, handler) {
+    let layer = {
+      path,
+      method: 'get',
+      handler,
+    };
+    app.routes.push(layer);
+  }
 
   app.listen = function (port, handler) {
     const server = http.createServer(app);
--- 10_express/server.js
+++ 10_express/server.js
@@ -6,11 +6,15 @@ app.get('/', function (req, res) {
   res.end('hello jerry.');
 })
 
-// 仅匹配一次，不重复执行handler。
-app.get('/', function (req, res) {
-  res.end('hello jerry, too.');
+app.post('/login', function(req, res) {
+  res.end('登录成功');
 })
 
+// 全局匹配中间件 all + *
+// 所有方法 + 所有路径
+app.all('*', function(req, res) {
+  res.end('404');
+})
 
 app.listen(4444, function () {
   console.info('[Server] http://127.0.0.1:4444');
