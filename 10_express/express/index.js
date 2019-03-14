f0e4d2a1f408b7a22fdbce365697efabb699a862
szy0syz <szy0syz@gmail.com>
2019-03-14 13:24:19 +0800

    Update 10_express_v4

--- 10_express/express/index.js
+++ 10_express/express/index.js
@@ -4,29 +4,54 @@ const methods = require('methods');
 
 function application() {
   let app = (req, res) => {
-    let { pathname } = url.parse(req.url); // 可以有请求参数等
-    let requestMethod = req.method.toLowerCase();
-    for (let i = 0; i < app.routes.length; i++) {
-      const layer = app.routes[i];
-      const { path, handler, method } = layer;
-      
-      // 为动态路由赋值
-      if (path.params) {
-        const matches = pathname.match(path);
-        if (matches) {
-          let [, ...lists] = matches;
-          req.params = path.params.reduce((memo, arg, index) => (memo[arg] = lists[index], memo), {});
-          return handler(req, res);
-        }
+    const { pathname } = url.parse(req.url); // 可以有请求参数等
+    const requestMethod = req.method.toLowerCase();
+
+    // 此时 routes：[use, use, route, route-404]
+    let index = 0;
+    function next(err) {
+      if (index >= app.routes.length) {
+        res.statusCode = 404;
+        return res.end(`Cannot ${requestMethod.toUpperCase()} ${pathname}`);
       }
 
-      if ((pathname === path || path === '*') && ((requestMethod === method) || method === 'all')) {
-        // 如果找到了对应的路径与请求方法，就执行回调并返回，不再往下找
-        return handler(req, res);
+      const { method, path, handler } = app.routes[index++];
+      if (err) {
+        if (method === 'middleware' && handler.length === 4) {
+          return handler(err, req, res, next);
+        } else {
+          next(err);
+        }
+      } else {
+        if (method === 'middleware') {
+          // 路径相同 和 路径是/ 和路径开头一样也匹配
+          // "/"  "/user" "/user/44/jerry"
+          if (pathname === path || path === '/' || pathname.startsWith(path + '/')) {
+            return handler(req, res, next); // 将next的执行方法交给用户 由用户决定是否向下执行
+          } else {
+            next();
+          }
+        } else {
+          // 为动态路由赋值
+          if (path.params) {
+            const matches = pathname.match(path);
+            if (matches) {
+              let [, ...lists] = matches;
+              req.params = path.params.reduce((memo, arg, index) => (memo[arg] = lists[index], memo), {});
+              return handler(req, res, next);
+            }
+          }
+  
+          if ((pathname === path || path === '*') && ((requestMethod === method) || method === 'all')) {
+            // 如果找到了对应的路径与请求方法，就执行回调并返回，不再往下找
+            return handler(req, res, next);
+          }
+  
+          next();
+        }
       }
     }
-    // 如果没有任何路由被匹配就执行 默认方法
-    res.end(`Cannot ${requestMethod.toUpperCase()} ${pathname}`);
+    next();
   }
 
   // 存路由
@@ -39,7 +64,7 @@ function application() {
       // 处理动态路由
       let params = [];
       if (path.includes(':')) {
-        path = path.replace(/:([^\/]*)/g, function() {
+        path = path.replace(/:([^\/]*)/g, function () {
           params.push(arguments[1]);
           return '([^\/]*)'
         });
@@ -53,7 +78,24 @@ function application() {
       };
       app.routes.push(layer);
     }
-  })
+  });
+
+  // 开始处理use中间件
+  app.use = function (path, handler) {
+    // 处理中间件参数，如果没传path，默认是 /
+    if (typeof handler !== 'function') {
+      // 如果 handler 不是函数，我们就认为没传专用的 监听函数，则认为path等于默认的 /，匹配全局
+      // 其实这个时候 path 就是我们要的handler，提前做
+      handler = path;
+      path = '/';
+    }
+    const layer = {
+      method: 'middleware',
+      path,
+      handler,
+    };
+    app.routes.push(layer);
+  }
 
   app.listen = function (port, handler) {
     const server = http.createServer(app);
--- 10_express/server.js
+++ 10_express/server.js
@@ -2,6 +2,24 @@ const express = require('./express');
 
 const app = express();
 
+// v4: 支持中间件
+/* 中间件可以做的事：
+  1. 权限验证
+  2. 配置公共方法和公共属性
+  3. 如果不调用next方法 则不会继续向下执行
+  4. 一般中间件都在路由之前
+  5. 如果中间件next方法中传递参数了 我们就认为这个参数是错误信息 需要不停往下传
+*/
+app.use(function(req, res, next) {
+  console.log('use 中间件');
+  next();
+});
+
+app.use('/user', function(req, res, next) {
+  console.log('/user 路由中间件');
+  next();
+})
+
 app.get('/', function (req, res) {
   res.end('hello jerry.');
 })
@@ -9,7 +27,8 @@ app.get('/', function (req, res) {
 // 路由参数： [id, name]  [44, jerry]
 // /user/:id/:name this.$route.params
 
-app.get('/user/:id/:name', function(req, res) {
+app.get('/user/:id/:name', function(req, res, next) {
+  next('用户名获取失败')
   res.end(JSON.stringify(req.params));
 })
 
@@ -21,6 +40,17 @@ app.post('/login', function(req, res) {
 // 所有方法 + 所有路径
 app.all('*', function(req, res) {
   res.end('404');
+})
+
+app.use(function(err, req, res, next) {
+  console.log('我是兜底函数1', err)
+  next(err)
+})
+
+app.use(function(err, req, res, next) {
+  console.log('我是兜底函数2')
+  console.log(err)
+  res.end('500~~~~' + err)
 })
 
 app.listen(4444, function () {
